ca2d1be22eae0b0f21b0d7160c9fe7d05d0e9506
Michael Wang <isVoid@users.noreply.github.com>
2022-06-23 14:23:01 -0700

    Add missing include guard

--- cpp/include/cuspatial/experimental/type_utils.hpp
+++ cpp/include/cuspatial/experimental/type_utils.hpp
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+#pragma once
+
 #include <thrust/iterator/transform_iterator.h>
 #include <thrust/iterator/transform_output_iterator.h>
 #include <thrust/iterator/zip_iterator.h>
